af06271a27c57fa26ad1fdf978f0d3b519fe62fa
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-10-22 11:22:22 +0200

    Fix CI

--- packages/css/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.css = void 0;
-var css_1 = require("@tokenami/css");
-var config_js_1 = require("./config.js");
-exports.css = (0, css_1.createCss)(config_js_1.config, { escapeSpecialChars: false });
